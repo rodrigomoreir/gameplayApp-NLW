e51ee4ef69327ffde92406da8ed01acda81d1bf4
Rodrigo Moreira <rodrigoalvesm06@gmail.com>
2021-06-22 20:33:54 -0300

    first day of NLW

---
+++ src/components/ButtonIcon/index.tsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import { View, Text, Image, TouchableOpacity, TouchableOpacityProps } from 'react-native'
+import { styles } from './styles'
+
+import DiscordImg from '../../assets/discord.png'
+
+interface Props extends TouchableOpacityProps {
+    title: string
+}
+
+export const ButtonIcon = ({title, ...rest}: Props) => {
+    return (
+        <TouchableOpacity style={styles.container} {...rest}>
+            <View style={styles.iconWrapper}>
+                <Image source={DiscordImg} style={styles.icon} />
+            </View>
+            <Text style={styles.title}>
+                {title}
+            </Text>
+        </TouchableOpacity>
+       
+    )
+}
